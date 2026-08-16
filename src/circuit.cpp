c3ecaea2187d17fb30a9f11ddaac7700adcff4c2
Riya Tiwari <anilendu007@gmail.com>
2026-08-16 14:10:36 +0530

    Refactor resistance matrix to use 1D vector

--- src/circuit.cpp
+++ src/circuit.cpp
@@ -6,7 +6,7 @@ namespace quant_eng {
 
 CircuitSolver::CircuitSolver(size_t num_loops) 
     : num_loops_(num_loops), 
-      resistance_matrix_(num_loops, std::vector<double>(num_loops, 0.0)), 
+      resistance_matrix_(num_loops * num_loops, 0.0), 
       voltage_vector_(num_loops, 0.0) {}
 
 void CircuitSolver::configure_loop(size_t loop_idx, const MeshLoop& loop_config) {
@@ -15,22 +15,25 @@ void CircuitSolver::configure_loop(size_t loop_idx, const MeshLoop& loop_config)
     }
 
     // Self-resistance forms the primary diagonal element
-    resistance_matrix_[loop_idx][loop_idx] = loop_config.total_resistance;
+    resistance_matrix_[loop_idx * num_loops_ + loop_idx] = loop_config.total_resistance;
     
     // Shared mutual resistances form symmetric off-diagonal elements (Negative mutual terms in KVL)
     for (const auto& [neighbor_idx, resistance] : loop_config.shared_resistances) {
         if (neighbor_idx >= num_loops_) {
             throw std::invalid_argument("Neighboring loop index is out of system bounds.");
         }
-        resistance_matrix_[loop_idx][neighbor_idx] = -resistance;
+        resistance_matrix_[loop_idx * num_loops_ + neighbour_idx] = -resistance;
     }
 
     voltage_vector_[loop_idx] = loop_config.total_voltage_source;
 }
 
 std::vector<double> CircuitSolver::calculate_currents() const {
-    // Forward mapping directly to the specialized numerical elimination solver
-    return MatrixSolver::solve_linear_system(resistance_matrix_, voltage_vector_);
+    // Create local modifiable copies since our solver optimizes using in-place modifications
+    std::vector<double> A_copy = resistance_matrix_;
+    std::vector<double> b_copy = voltage_vector_;
+
+    return MatrixSolver::solve_linear_system(A_copy, b_copy, num_loops_);
 }
 
 } // namespace quant_eng
